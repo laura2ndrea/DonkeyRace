e7770cb7321ef6e479244277236de0c1535d9269
Laura Andrea Rodríguez Mejía <91057900+laura2ndrea@users.noreply.github.com>
2025-01-13 07:39:35 -0500

    Update insertionsDatabase.sql

--- insertionsDatabase.sql
+++ insertionsDatabase.sql
@@ -1,8 +1,5 @@
-
--- Seleccionar la base de datos
 USE DonkeyRace;
 
--- Inserciones para la tabla Breeds
 INSERT INTO Breeds (name) VALUES 
 ('Andalusian'),
 ('Catalonian'),
@@ -10,7 +7,6 @@ INSERT INTO Breeds (name) VALUES
 ('Poitevin Donkey'),
 ('American Mammoth');
 
--- Inserciones para la tabla Owners
 INSERT INTO Owners (name) VALUES 
 ('John Doe'),
 ('Jane Smith'),
@@ -18,7 +14,6 @@ INSERT INTO Owners (name) VALUES
 ('Emily Davis'),
 ('Alice Johnson');
 
--- Inserciones para la tabla Donkeys
 INSERT INTO Donkeys (name, birthdate, id_breed, id_owner) VALUES 
 ('Speedy', '2015-06-15', 1, 1),
 ('Lightning', '2018-08-22', 2, 2),
@@ -26,7 +21,6 @@ INSERT INTO Donkeys (name, birthdate, id_breed, id_owner) VALUES
 ('Blaze', '2017-03-10', 4, 4),
 ('Storm', '2020-01-25', 5, 5);
 
--- Inserciones para la tabla Racing
 INSERT INTO Racing (race_date, place, status) VALUES 
 ('2025-02-15 14:00:00', 'Zone 1', 'Scheduled'),
 ('2025-02-20 16:00:00', 'Zone 2', 'Scheduled'),
@@ -34,7 +28,6 @@ INSERT INTO Racing (race_date, place, status) VALUES
 ('2025-03-01 12:00:00', 'Zone 1', 'Scheduled'),
 ('2025-03-05 18:00:00', 'Zone 2', 'Scheduled');
 
--- Inserciones para la tabla Registrations
 INSERT INTO Registrations (race_position, id_donkey, id_race) VALUES 
 (0, 1, 1),
 (0, 2, 1),
@@ -42,7 +35,6 @@ INSERT INTO Registrations (race_position, id_donkey, id_race) VALUES
 (0, 4, 2),
 (0, 5, 3);
 
--- Inserciones para la tabla Spectators
 INSERT INTO Spectators (name, identification, credit) VALUES 
 ('Carlos Gonzalez', 'ID123456', 100.00),
 ('Maria Rodriguez', 'ID654321', 200.00),
@@ -50,7 +42,6 @@ INSERT INTO Spectators (name, identification, credit) VALUES
 ('Ana Gomez', 'ID333444', 300.00),
 ('Sofia Martinez', 'ID555666', 150.00);
 
--- Inserciones para la tabla Bets
 INSERT INTO Bets (amount, id_spectator, id_registration) VALUES 
 (20.00, 1, 1),
 (30.00, 2, 2),
